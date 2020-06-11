3d8a6a993b50406b5538a165e901ad4927ae325a
andy <otarolaandy@gmail.com>
2020-06-11 11:31:01 -0500

    v.1.0.0

--- src/js/components/nabvar.js
+++ src/js/components/nabvar.js
@@ -1,71 +1,38 @@
 function activeNavbar(){
-   let navbars = Array.from(document.querySelectorAll('[data-toggle=navbar]'));
-   let navbars_sublist = Array.from(document.querySelectorAll('[data-toggle=navbar_sublist]'));
+   let navbarBtn = document.querySelector('[data-toggle=navbar]');
+   let navbars_btn_sublist = Array.from(document.querySelectorAll('[data-toggle=navbar_sublist]'));
    let backdrop = document.createElement("div");
    let navbar = null;
-   
-   if(navbars.length > 0){
-      navbars.forEach((el,i)=>{
-         let btn = el;
-         let data_target = el.dataset.target;       
-         let data_toggle = el.dataset.toggle;
 
-         navbar = document.querySelector(`${data_target}`);
-         
-         // Altura de la lista del navbar
-         let navbar_height = 0;
-
-         // Almacena la altura de la lista del navbar en la variable "navbar_height".Lo almacena calculando la suma de todos los items que están detro de la lista del navbar
-         let navbar_items = Array.from(navbar.children).map((element)=>{
-            navbar_height +=  element.scrollHeight
-            return el.scrollHeight;
-         })
-   
-         if(data_toggle === 'navbar'){
-            btn.addEventListener('click',()=>{   
-               navbar.classList.toggle('navbar--active');
-               
-               if(document.body.outerHTML.includes(backdrop.outerHTML)){
-                  document.body.removeChild(backdrop);
-               }else{
-                  backdrop.classList.add("backdrop")
-                  backdrop.style.zIndex = "10";
-                  document.body.appendChild(backdrop);
-               }
-            })         
-         }
-
-      })
-
-   }
-
-   // Mostrar sublista del navbar
-
-   if(navbars_sublist.length > 0){
-      navbars_sublist.forEach((el,i)=>{
-         let btn = el;
-         let data_target  = el.getAttribute('data-target');       
-         let data_toggle  = el.getAttribute('data-toggle');       
-         let navbar_list = document.querySelector(data_target);
-         
-         btn.addEventListener('click',(e)=>{
-            e.preventDefault();
-            if(innerWidth < 1024){
-               if(data_toggle === 'navbar_sublist'){
-                  if(navbar_list.style.display === "" || navbar_list.style.display === "none"){
-                     navbar_list.style.display = "block";    
-                  }else{
-                     navbar_list.style.display = "none";
-                  }
-               }
-            }
-            
-         })         
+   if(navbarBtn){
+      let data_target = navbarBtn.dataset.target;       
       
-      })
+      navbarBtn.addEventListener('click',(e)=>{      
+         navbar = document.querySelector(`${data_target}`);
+         navbar.classList.toggle('navbar--active');
+
+         if(document.body.outerHTML.includes(backdrop.outerHTML)){
+            document.body.removeChild(backdrop);
+         }else{
+            backdrop.classList.add("backdrop")
+            backdrop.style.zIndex = "10";
+            document.body.appendChild(backdrop);
+         }
+      })         
+   
    }
 
-   // Quitar el navbar, si el innerHTML del click no está dentro del innerHTML del contenedor "div.navbar__list"
+   if(navbars_btn_sublist.length > 0){
+      navbars_btn_sublist.forEach((el)=>{
+         let btnNavbarSublist = el;
+         let navbar_sublist = document.querySelector(`${el.dataset.target}`);
+         btnNavbarSublist.addEventListener("click", ()=>{
+            if(innerWidth < 1024){
+               navbar_sublist.classList.toggle("navbar__sublist--active");
+            }
+         })
+      })
+   }
    
    const removeMenu = ()=>{
       navbar.classList.remove('navbar--active')
@@ -78,18 +45,27 @@ function activeNavbar(){
       if(document.body.outerHTML.includes(backdrop.outerHTML) && innerWidth >= 1024 && navbar != null){
          document.body.removeChild(backdrop);
          navbar.classList.remove('navbar--active')
+         navbars_btn_sublist.forEach((el)=>{
+            let navbar_sublist = document.querySelector(`${el.dataset.target}`);
+            navbar_sublist.classList.remove("navbar__sublist--active")
+         })
       }
    })
 
    window.addEventListener('click', (e)=>{
-      navbars.forEach((el,i)=>{
-         if(!el.parentElement.innerHTML.includes(e.target.innerHTML)){
-            removeMenu();
+      if(e.target != navbarBtn && navbar){
+         if(!navbar.innerHTML.includes(e.target.innerHTML)){
+            removeMenu();      
+         }else if(e.target.href){
+            if(!e.target.href.includes("#")){
+               removeMenu();      
+            }
          }
          if(e.target.outerHTML.includes("backdrop")){
             removeMenu();
          }
-      })
+      }
+      
    })
 }
 
--- src/js/components/ripple.js
+++ src/js/components/ripple.js
@@ -1,52 +1,96 @@
 function ripples(){
    
-     let ripples_btn = Array.from(document.querySelectorAll("[class*='ripple']"));
-     let nowRippleActive = null;
-     let nowRippleAnimate = null;
+   let ripples_btn = Array.from(document.querySelectorAll("[class*='ripple']"));
+   let nowRippleActive = null;
+   let nowRippleAnimate = null;
+   let userAgent = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+   window.addEventListener("resize", ()=>{
+      userAgent =  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) 
+   })
+
+   window.addEventListener("mousedown",(e)=>{
+      if(nowRippleAnimate && nowRippleActive){
+
+         document.querySelectorAll("[class*=ripple--active]").forEach((ripple_active)=>{
+            if(ripple_active !== nowRippleActive){
+               removeRippleAnimation(ripple_active)               
+            }
+         })
+      }
+   })
+
+   window.addEventListener("touchstart",()=>{
+      if(nowRippleAnimate && nowRippleActive){
+         document.querySelectorAll("[class*=ripple--active]").forEach((ripple_active)=>{
+            if(ripple_active !== nowRippleActive){
+               removeRippleAnimation(ripple_active)               
+            }
+         })
+      }
+   })
 
    ripples_btn.forEach((ripple)=>{
-
-      window.addEventListener("click",(e)=>{
-         if(nowRippleAnimate && nowRippleActive && !e.target.className.includes("ripple")){
-            removeRippleAnimation(nowRippleActive, nowRippleAnimate)
-         }
-      })
-
+      
       ripple.addEventListener("mousedown",(e)=>{
-         addRippleAnimation(ripple, e);
+         if(!userAgent){
+            addRippleAnimation(ripple, e.clientX,  e.clientY);
+         }
       })
 
       ripple.addEventListener("mouseup", ()=>{
-         if(nowRippleAnimate && nowRippleActive){
-            removeRippleAnimation(nowRippleActive, nowRippleAnimate)
+         if(nowRippleAnimate && nowRippleActive && !userAgent){
+            removeRippleAnimation(nowRippleActive)
          }
       })
 
-      let count =  0;
+      let count_key_press =  0;
 
       ripple.addEventListener("keypress", (e)=>{
-         if(e.keyCode === 13 && count === 0){
-            addRippleAnimation(ripple, e);
+         if(!userAgent){
+            if((e.keyCode === 13 && count_key_press === 0) || 
+               (e.keyCode === 32 && count_key_press === 0)){
+               addRippleAnimation(ripple, e.clientX,  e.clientY);
+            }
+            count_key_press++;
          }
-         count++;
       })
 
       ripple.addEventListener("keyup", (e)=>{
-         if(e.keyCode === 13 && nowRippleAnimate && nowRippleActive){
-            count = 0;
-            removeRippleAnimation(nowRippleActive, nowRippleAnimate)
+         if(!userAgent){
+            if((e.keyCode === 13 && nowRippleAnimate && nowRippleActive) ||
+               (e.keyCode === 32 && nowRippleAnimate && nowRippleActive)
+            ){
+               count_key_press = 0;
+               removeRippleAnimation(nowRippleActive)
+            }
          }
       })
 
       ripple.addEventListener("mouseleave", ()=>{
-         if(nowRippleAnimate && nowRippleActive){
-            removeRippleAnimation(nowRippleActive, nowRippleAnimate)
+         if(nowRippleAnimate && nowRippleActive && !userAgent){
+            removeRippleAnimation(nowRippleActive)
          }
       })
-      
-   })
 
-   const addRippleAnimation = (ripple, e)=>{
+      ripple.addEventListener("touchstart",(e)=>{            
+         addRippleAnimation(ripple, e.touches[0].clientX,  e.touches[0].clientY);
+      })
+      
+      ripple.addEventListener("touchend",(e)=>{
+         if(nowRippleAnimate && nowRippleActive){
+            removeRippleAnimation(nowRippleActive)               
+         }
+      })
+
+      ripple.addEventListener("touchcancel",(e)=>{
+         if(nowRippleAnimate && nowRippleActive){
+            removeRippleAnimation(nowRippleActive)
+         }
+      })
+   })
+   
+   const addRippleAnimation = (ripple, x, y)=>{
       let ripple_active = document.createElement("span");
       let ripple_animate = document.createElement("span");
       let rippleHeight = ripple.getBoundingClientRect().height;
@@ -54,9 +98,9 @@ function ripples(){
       let animateSize = Math.sqrt(Math.pow((rippleHeight / 2), 2) + Math.pow((rippleWidth / 2), 2)) * 2;
       let yAxis = (animateSize - rippleHeight) / 2;
       let xAxis = (animateSize - rippleWidth) / 2;
-      let moveX = e.clientX - ripple.getBoundingClientRect().left; 
-      let moveY = e.clientY - ripple.getBoundingClientRect().top;
-      let size =  parseInt(Math.min(ripple.offsetWidth, ripple.offsetHeight) * 0.5);
+      let moveX = x - ripple.getBoundingClientRect().left; 
+      let moveY = y - ripple.getBoundingClientRect().top;
+      let size =  Math.min(ripple.offsetWidth, ripple.offsetHeight)*0.5;
       nowRippleActive = ripple_active;
       nowRippleAnimate = ripple_animate;
 
@@ -64,31 +108,33 @@ function ripples(){
       ripple_animate.classList.add("ripple--animate");         
       ripple.appendChild(ripple_active);  
       ripple_active.appendChild(ripple_animate);
+
       ripple_animate.style.opacity = "0"
-      ripple_animate.style.transform = `translate(${moveX}px, ${moveY}px)`   
       ripple_animate.style.height = size + "px"
       ripple_animate.style.width = size + "px"
       
+      ripple_animate.style.transform = `translate(${moveX - size*0.5}px, ${moveY - size*0.5}px)`   
+      
       
       setTimeout(()=>{
+         ripple_animate.style.opacity = ".2";
          ripple_animate.style.height = animateSize + "px";
          ripple_animate.style.width = animateSize + "px";
-         ripple_animate.style.transform = `translate(-${xAxis}px, -${yAxis}px) scale3d(1, 1, 1)`;               
-         ripple_animate.style.opacity = ".2";
-      }, .0001)
+         ripple_animate.style.transform = `translate(-${xAxis}px, -${yAxis}px) scale3d(1, 1, 1)`;         
+      }, .001)
 
    }
 
-   const removeRippleAnimation = (rippleActive, rippleAnimate)=>{
+   const removeRippleAnimation = (rippleActive)=>{
       setTimeout(()=>{
          rippleActive.style.opacity = "0";
-      }, 200)
+      }, 260)
       
       setTimeout(()=>{
          rippleActive.remove();
-      }, 330)
-   }
+      }, 360)
 
+   }
 }
 
 
